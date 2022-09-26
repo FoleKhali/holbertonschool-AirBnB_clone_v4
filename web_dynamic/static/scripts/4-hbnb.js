e04f8caec852703355aac846b347fc29f3d83a0b
FoleKhali <dhannaps03@gmail.com>
2022-09-25 23:13:40 -0500

    ✅ adding script for task 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -48,4 +48,55 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         }
       });
+      $('button').click(function () {
+        listAmeny = Object.keys(myDict);
+        datas = { amenities: listAmeny };
+        console.log(listAmeny);
+        datas = JSON.stringify(datas);
+        $('SECTION.places').empty();
+        $.ajax({
+          url: 'http://0.0.0.0:5001/api/v1/places_search/',
+          contentType: 'application/json',
+          type: 'POST',
+          data: datas,
+          success: function (data) {
+            data.sort(function (a, b) {
+              if (a.name > b.name) {
+                return 1;
+              }
+              if (a.name < b.name) {
+                return -1;
+              }
+              return 0;
+            });
+            for (const elem of Object.values(data)) {
+              $.ajax({
+                url: 'http://0.0.0.0:5001/api/v1/users/' + elem.user_id,
+                contentType: 'application/json',
+                type: 'GET',
+                success: function (dataname) {
+                  $('SECTION.places').append(
+                    '<article> <div class="title">' +
+                      '<h2>' + elem.name + '</h2>' +
+                      '<div class="price_by_night">' + elem.price_by_night + '</div> </div>' +
+                      '<div class="information"> <div class="max_guest">' +
+                      '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+                      '<br />' + elem.max_guest + ' Guests' + '</div>' +
+                      '<div class="number_rooms">' +
+                      '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+                      '<br />' + elem.number_rooms + ' Bedrooms' + '</div>' +
+                      '<div class="number_bathrooms">' +
+                      '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+                      '<br />' + elem.number_bathrooms + ' Bathroom' + '</div>' + '</div>' +
+                      '<div class="user">' + '<strong>' + 'Owner: ' + dataname.first_name + ' ' + dataname.last_name + '</strong>' + '</div>' +
+                      '</div>' + '<div class="description">' + elem.description + '</div>' +
+  '</article>'
+                  );
+                }
+              });
+            }
+          }
+        });
+      });
     });
+  });
